e54b112bb6fb63ccb49c78a134cc5848e8db80f0
etj <etj@geo-solutions.it>
2022-12-16 12:17:02 +0100

    L&F WIP

--- ckanext/liguria/plugin.py
+++ ckanext/liguria/plugin.py
@@ -10,5 +10,4 @@ class LiguriaPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'liguria')
+        toolkit.add_resource('assets', 'liguria')  # path, webasset name
